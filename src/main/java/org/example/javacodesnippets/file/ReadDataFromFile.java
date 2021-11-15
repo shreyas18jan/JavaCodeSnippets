17fda320f429dca34841c004ab1f707c4c79c317
shreyas18jan <shreyas18jan@gmail.com>
2021-11-15 06:44:18 +0530

    Read file data using java.nio.file.Files class with lines method

--- src/main/java/org/example/javacodesnippets/file/ReadDataFromFile.java
+++ src/main/java/org/example/javacodesnippets/file/ReadDataFromFile.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class ReadDataFromFile {
     // Version-1 to get the file Path : Using getResource() method
@@ -15,27 +16,46 @@ public class ReadDataFromFile {
     public static void main(String[] args) throws IOException {
         ReadDataFromFile readDataFromFile = new ReadDataFromFile();
 
-        // Example - 1 : Easy way to read files is through java.nio.file.Files class
-        readDataFromFile.readAllLinesFromFilesClass();
+        // Example - 1.1 : Read file data using java.nio.file.Files class with readAllLines method
+        readDataFromFile.usingReadAllLinesFromFilesClass();
+
+        // Example - 1.2 : Read file data using java.nio.file.Files class with lines method
+        readDataFromFile.usingLinesMethodFromFilesClass();
+
+        // Compared to Files.readAllLines() vs Files.lines() :
+        // readAllLines() - is preferred when the file is small.
+        // lines() - is preferred when the file is large.
 
     }
 
     /*
-        Example - 1 : Easy way to read files is through java.nio.file.Files class
+        Example - 1.1 : Read file data using java.nio.file.Files class with readAllLines method
      */
-    public void readAllLinesFromFilesClass() throws IOException {
+    public void usingReadAllLinesFromFilesClass() throws IOException {
+        // Paths.get() : Converts String to Path object that can locate a file in the file system.
         // Files.readAllLines() : returns list of Strings took from file. Each entry in the List is a line from the file.
         // String.join() : Can join the String type iterable with the specified delimiter.
         String fileNameV1Content = String.join("", Files.readAllLines(Paths.get(this.fileNameV1.getFile()), StandardCharsets.UTF_8));
         String fileNameV2Content = String.join("", Files.readAllLines(Paths.get(this.fileNameV2), StandardCharsets.UTF_8));
 
-        if(fileNameV1Content.equals(fileNameV2Content)) {
-            System.out.println(" File content has to be same!");
+        compareStringContent(fileNameV1Content, fileNameV2Content);
+    }
 
-        } else {
-            System.out.println("Something went wrong!");
-        }
+    /*
+        Example - 1.2 : Read file data using java.nio.file.Files class with lines method
+     */
+    public void usingLinesMethodFromFilesClass() throws IOException {
+        // Paths.get() : Converts String to an object that can locate a file in the file system.
+        // Files.lines() : returns Stream of Strings took from file. Each entry in the entry is a line from the file.
+        // Collectors.joining() : Takes Stream of String and joins it based on the delimiter, by default join as it is.
+        String fileNameV1Content = Files.lines(Paths.get(this.fileNameV1.getFile()), StandardCharsets.UTF_8).collect(Collectors.joining());
+        String fileNameV2Content = Files.lines(Paths.get(this.fileNameV2), StandardCharsets.UTF_8).collect(Collectors.joining());
 
+        compareStringContent(fileNameV1Content, fileNameV2Content);
+    }
+
+    public void compareStringContent(String fileNameV1Content, String fileNameV2Content) {
+        System.out.println((fileNameV1Content.equals(fileNameV2Content))?" File content has to be same!":"Something went wrong!");
         System.out.println("fileNameV1Content : " + fileNameV1Content);
         System.out.println("fileNameV2Content : " + fileNameV2Content);
     }
