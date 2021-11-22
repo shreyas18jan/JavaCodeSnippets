270e17de2d9e3e743a9ae6be95fa1d69cba111c0
shreyas18jan <shreyas18jan@gmail.com>
2021-11-22 09:31:33 +0530

    Added BufferedReader and Scanner example for reading data from file

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ This repository will have few code snippets that can help other for a quick chec
 Code Snippet and comments are also added for better understanding.
 <li>Using Files.readAllLines</li>
 <li>Using Files.lines</li>
+<li>Using Files.readAllBytes</li>
+<li>Using BufferedReader</li>
+<li>Using Scanner</li>
+
+There are other classes to read data from a file, like ByteArray-Based and Character-Based.
+Above are the easy once to use in any framework if there is a need for reading file.
 
 You can refer this file - [ReadDataFromFile](./src/main/java/org/example/javacodesnippets/file/ReadDataFromFile.java)
--- src/main/java/org/example/javacodesnippets/file/ReadDataFromFile.java
+++ src/main/java/org/example/javacodesnippets/file/ReadDataFromFile.java
@@ -1,15 +1,17 @@
 package org.example.javacodesnippets.file;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class ReadDataFromFile {
     // Version-1 to get the file Path : Using getResource() method
     private final URL fileNameV1 = this.getClass().getClassLoader().getResource("ReadDataFromFile_TextFile.txt");
+
     // Version-2 to get the file Path : Using System.getProperty("user.dir") method
     private final String fileNameV2 = System.getProperty("user.dir") + "/src/main/resources/ReadDataFromFile_TextFile.txt";
 
@@ -22,9 +24,19 @@ public class ReadDataFromFile {
         // Example - 1.2 : Read file data using java.nio.file.Files class with lines method
         readDataFromFile.usingLinesMethodFromFilesClass();
 
+        // Example - 1.3 : Read file data using java.nio.file.Files class with readAllBytes method
+        readDataFromFile.usingReadAllBytesMethodFromFilesClass();
+
         // Compared to Files.readAllLines() vs Files.lines() :
         // readAllLines() - is preferred when the file is small.
         // lines() - is preferred when the file is large.
+        // readAllBytes() - is preferred when the file type is Binary Data or Image Data.
+
+        // Example - 2.1 : Read file data using BufferedReader class with readLine method
+        readDataFromFile.usingReadLineMethodFromBufferedReaderClass();
+
+        // Example - 2.2 : Read file data using Scanner class with nextLine method
+        readDataFromFile.usingNextLineMethodFromScannerClass();
 
     }
 
@@ -52,6 +64,66 @@ public class ReadDataFromFile {
         String fileNameV2Content = Files.lines(Paths.get(this.fileNameV2), StandardCharsets.UTF_8).collect(Collectors.joining());
 
         compareStringContent(fileNameV1Content, fileNameV2Content);
+    }
+
+    /*
+        Example - 1.3 : Read file data using java.nio.file.Files class with readAllBytes method
+     */
+    public void usingReadAllBytesMethodFromFilesClass() throws IOException {
+        // Paths.get() : Converts String to an object that can locate a file in the file system.
+        // Files.readAllBytes() : Reads all the bytes from a file. Similar to Files.readAllLines(), but not bound to String type.
+        String fileNameV1Content = new String(Files.readAllBytes(Paths.get(this.fileNameV1.getFile())));
+        String fileNameV2Content = new String(Files.readAllBytes(Paths.get(this.fileNameV2)));
+
+        compareStringContent(fileNameV1Content, fileNameV2Content);
+    }
+
+    /*
+        Example - 2.1 : Read file data using BufferedReader class with readLine method
+     */
+    public void usingReadLineMethodFromBufferedReaderClass() throws IOException {
+        // Paths.get() : Converts String to an object that can locate a file in the file system.
+        // FileReader : Extends InputStream which is used for reading character files.
+        // BufferedReader : Reads text from a Reader/InputStream, buffers characters for efficient reading of characters, arrays, and lines.
+        FileReader fileOneReader = new FileReader(this.fileNameV1.getFile());
+        BufferedReader fileNameV1ContentReader = new BufferedReader(fileOneReader);
+
+        FileReader fileTwoReader = new FileReader(this.fileNameV2);
+        BufferedReader fileNameV2ContentReader = new BufferedReader(fileTwoReader);
+
+        StringBuilder fileNameV1Content = new StringBuilder();
+        String line;
+        while((line=fileNameV1ContentReader.readLine()) != null) {
+            fileNameV1Content.append(line);
+        }
+
+        StringBuilder fileNameV2Content = new StringBuilder();
+        while((line=fileNameV2ContentReader.readLine()) != null) {
+            fileNameV2Content.append(line);
+        }
+
+        compareStringContent(fileNameV1Content.toString(), fileNameV2Content.toString());
+    }
+
+    /*
+        Example - 2.2 : Read file data using Scanner class with nextLine method
+     */
+    private void usingNextLineMethodFromScannerClass() throws FileNotFoundException {
+        // Scanner : A simple text scanner which can parse primitive types and strings using regular expressions.
+        Scanner fileNameV1Scanner = new Scanner(new File(this.fileNameV1.getFile()));
+        StringBuilder fileNameV1Content = new StringBuilder();
+        Scanner fileNameV2Scanner = new Scanner(new File(this.fileNameV2));
+        StringBuilder fileNameV2Content = new StringBuilder();
+
+        while(fileNameV1Scanner.hasNextLine()) {
+            fileNameV1Content.append(fileNameV1Scanner.nextLine());
+        }
+
+        while(fileNameV2Scanner.hasNextLine()) {
+            fileNameV2Content.append(fileNameV2Scanner.nextLine());
+        }
+
+        compareStringContent(fileNameV1Content.toString(), fileNameV2Content.toString());
     }
 
     public void compareStringContent(String fileNameV1Content, String fileNameV2Content) {
